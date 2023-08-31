2eef4f1624c78656860a2793397fe0831b93d042
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-31 10:44:38 +0530

    route: /todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const express = require('express');
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+	res.send("Hello world");
+})
+
+app.get("/todos", (req, res) => {
+	res.send("Get todos!");
+})
+
+app.listen(PORT, () => {
+	console.log("app running on localhost:3000");
+})
+
+/*
+ * nodejs api are quite low level and most people use packages to make it easier
+ *
+ *
+ */
